fd713de4ab0453c255f438b6b8d5b96baaa37d9f
abayo-luc <luc.bayo@gmail.com>
2019-11-22 16:40:18 +0200

    feature: testing plugin
    - Add testing option
    - Add moch and jest test configuration
    - Add both mocha and jest test samples
    
    [Finishes #testing]

--- templates/javascript/app.js
+++ templates/javascript/app.js
@@ -2,7 +2,6 @@ import app from './index'
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => {
+app.listen(PORT, () =>
     process.env.NODE_ENV === 'development' &&
-        console.log(`Listening on port ${PORT}`)
-})
+    console.log(`Listening on port ${PORT}`))
